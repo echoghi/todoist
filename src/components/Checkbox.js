029a8d96eda3bb73ee19968c13a462e315c654d0
emilec <emchoghi@gmail.com>
2020-05-08 10:26:21 -0700

    checkbox hover effect

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Firebase from '../firebase';
 import styled from 'styled-components';
+import { FaCheck } from 'react-icons/fa';
 
 export const Container = styled.div`
     cursor: pointer;
@@ -12,6 +13,27 @@ export const Container = styled.div`
     margin-right: 1rem;
     border: 1px solid gray;
     border-radius: 16px;
+    transition: 0.2s all ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+        opacity: 0;
+        transition: 0.2s opacity ease;
+        height: 7px;
+        width: 7px;
+        fill: gray;
+        margin-top: 1px;
+    }
+
+    &:hover {
+        background-color: hsla(0, 0%, 50.2%, 0.2);
+
+        svg {
+            opacity: 1;
+        }
+    }
 `;
 
 const Checkbox = ({ id }) => {
@@ -21,7 +43,11 @@ const Checkbox = ({ id }) => {
         });
     };
 
-    return <Container data-testid="checkbox" onClick={archiveTasks}></Container>;
+    return (
+        <Container data-testid="checkbox" onClick={archiveTasks}>
+            <FaCheck />
+        </Container>
+    );
 };
 
 export default Checkbox;
